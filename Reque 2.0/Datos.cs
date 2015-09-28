20f41c40eca1655d9d57c45ce9dc34f888314525
leomendoza123 <leomendoza123@gmail.com>
2015-09-28 12:28:20 -0600

    Inicio con git

---
+++ Reque 2.0/Datos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Collections;
+
+namespace Reque_2._0
+{
+    public class Datos
+    {
+        static string conString = "Server=localhost;Database=reque;Uid=root;";
+        static MySqlConnection conn = new MySqlConnection(conString);
+        public static List<Evento> GetEventos()
+        {
+            List<Evento> eventos = new List<Evento>();
+            
+            MySqlCommand command = conn.CreateCommand();
+            command.CommandText = "SELECT   `id`,  `name`,  `start`,  `end`,  `category`FROM  `eventos`; ";
+
+            try
+            {
+                MySqlDataReader reader = null;
+                conn.Open();
+                reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+                    Evento Evento = new Evento(
+                        Convert.ToInt32(reader["id"]),
+                        reader["name"].ToString(),
+                        Convert.ToDateTime(reader["start"]),
+                        Convert.ToDateTime(reader["end"]),
+                        reader["category"].ToString());
+
+                    eventos.Add(Evento);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            conn.Close();
+            return (eventos);
+
+        }
+                
+
+
+    }
+}
---
+++ Reque 2.0/WebService.asmx.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Services;
+
+
+namespace Reque_2._0
+{
+    /// <summary>
+    /// Descripción breve de WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class WebService : System.Web.Services.WebService
+    {
+
+      
+        [WebMethod]
+        public string  Eventos()
+        {
+            List<Evento> eventos = Datos.GetEventos();
+          //  return eventos.ToString();
+            return JsonConvert.SerializeObject(eventos, Formatting.Indented);
+     
+
+        }
+
+    }
+}
